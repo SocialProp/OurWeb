06135a478cdb8a818066ba5d848a405691efc741
Hernan bessera <hernanman_1@hotmail.com>
2016-12-26 12:42:13 -0300

    cambios en php mailer

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,26 +1,21 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
+require 'vendor/phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
+//$mail->SMTPDebug = 3;
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->setFrom( $_POST['your-email'], $_POST['your-name']);
+$mail->addAddress('Palomatati@hotmail.com');               // Name is optional
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+$mail->isHTML(true);                                  // Set email format to HTML
 
-// Create the email and send the message
-$to = 'hernanman_1@hotmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
-return false;
-?>
+$mail->Subject = 'Mail de la web!' ;
+$mail->Body    = $_POST['your-message'];
+$mail->AltBody = $_POST['your-message'];
+
+ if(!$mail->send()) {
+     echo 'Message could not be sent.';
+     echo 'Mailer Error: ' . $mail->ErrorInfo;
+ } else {
+   header('Location: index.html');
+   exit;
+}
