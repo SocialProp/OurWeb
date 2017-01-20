3f8c73296e5d1aaa9509089c032f5e432a3400b0
Manukir <manuelkir90@gmail.com>
2017-01-20 11:52:30 -0300

    agregogoogleanalytics

--- index.php
+++ index.php
@@ -467,7 +467,7 @@
             <div class="row">
                 <div class="col-xs-12 col-lg-4 col-lg-offset-4 text-center">
                     <h2 class="section-heading" id="textocontactanos">Contactanos</h2>
-                    <h3 class="section-subheading text-muted">En que te podemos ayudar.</h3>
+                    <h3 class="section-subheading text-muted">¿Cómo te podemos ayudar?</h3>
                 </div>
             </div>
             <div class="row">
@@ -741,6 +741,16 @@
 
             var mixer = mixitup(containerEl);
     </script>
+    <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-90674020-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 
 </html>
